948ecae96491efc8427d4cb52b52b1e6369ae2b8
Artyom Sukharev <sukharevartyom@yandex.ru>
2020-03-20 09:36:00 +0300

    добавлена сортировка

--- src/app/weather-list/weather-list.component.ts
+++ src/app/weather-list/weather-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'weather-list',
@@ -10,8 +10,33 @@ export class WeatherListComponent {
 
   constructor() { }
 
-  ngOnInit() {
-    console.log('DAYZZ: ', this.days);
+  public sort(by: string, direction: string): void {
+    const sortingMethodsDispatch = {
+      date: () => this.sortByDate(direction),
+      temperature: () => this.sortByTemperature(direction),
+    };
+
+    sortingMethodsDispatch[by]();
   }
 
+  private sortByDate(direction: string): void {
+    const compareFn = this.getCompareFn(direction);
+
+    this.days = this.days.slice().sort((a, b) => compareFn(new Date(a.dt_txt), new Date(b.dt_txt)));
+  }
+
+  private sortByTemperature(direction: string): void {
+    const compareFn = this.getCompareFn(direction);
+
+    this.days = this.days.slice().sort((a, b) => compareFn(a.main.temp, b.main.temp));    
+  }
+
+  private getCompareFn(direction: string): (a: any, b: any) => number {
+    const callbackDispatch = {
+      forward: (a, b) => a - b,
+      backwards: (a, b) => b - a,
+    };
+
+    return callbackDispatch[direction];
+  }
 }
